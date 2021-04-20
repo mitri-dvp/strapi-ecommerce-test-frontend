c609f8b93666c655bff4ddaec7d5c1d3970bf1c5
mitri-dvp <mitri.dvp@gmail.com>
2021-04-20 18:48:37 -0300

    Powered By Updated

--- components/PoweredBy.js
+++ components/PoweredBy.js
@@ -3,9 +3,7 @@ import styles from '../styles/PoweredBy.module.css'
 export default function PoweredBy() {
   return (
     <>
-      <a href="#" className={styles.powered_by}>
-        <p>powered by</p>
-        {/* <img src="/mitri-logo.png" alt="mitri.dvp"/> DEV */}
+      <a href="https://www.mitri-dvp.com/" className={styles.powered_by}>
         <img src="https://res.cloudinary.com/dz5vyxfew/image/upload/v1611380093/mitri-logo_vctatz.png" alt="mitri.dvp"/>
         <p>mitri.dvp</p>
       </a>
